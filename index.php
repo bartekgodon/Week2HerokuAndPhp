cff6ccf6ca43504598b8f17a01f6b84afb0674d1
bartek.godon <bartek.godon@dai.co.uk>
2021-01-28 14:54:44 +0000

    Added cool stuff

--- index.php
+++ index.php
@@ -3,4 +3,6 @@
 
     echo '<h1>HELLO WORLD!!!</h1>';
     echo '<p>This is a "Hello World" program in PHP</p>';
+
+    echo '<img src="https://thumbs.gfycat.com/BoldFlickeringAmericanshorthair-size_restricted.gif">';
 ?>
