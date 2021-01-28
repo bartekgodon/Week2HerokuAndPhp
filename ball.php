03160c138f205e0233611c4a8b29d1e48cec01cf
bartek.godon <bartek.godon@dai.co.uk>
2021-01-28 15:35:59 +0000

    Updated gif address

--- ball.php
+++ ball.php
@@ -1,7 +1,7 @@
 <?php
     include 'navigation.php';
 
-    echo '<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Ftrackxperience.com%2Fwwjd%2F&psig=AOvVaw1zg1tF1ZdEkjlyK3FBPI3f&ust=1611934216477000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPDiiIj5vu4CFQAAAAAdAAAAABAJ">';
+    echo '<img src="https://media0.giphy.com/media/3oEduFJVCzDPhr20WQ/giphy.gif">';
 
     include('hate_ball_frontend.html');
 ?>
